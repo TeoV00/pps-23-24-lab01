d27c6de454803daaf039922ba85a193f66376bbe
Matteo <matteo.violani@studio.unibo.it>
2024-02-26 11:34:05 +0100

    feat(CircularListTest, CircularListImpl): initial implementatio and test

--- tdd-exercise/src/test/java/CircularListTest.java
+++ tdd-exercise/src/test/java/CircularListTest.java
@@ -1,17 +1,32 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import tdd.CircularList;
+import tdd.CircularListImpl;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
+    private CircularList circularList;
 
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    @BeforeEach
+    void beforeEach() {
+        circularList = new CircularListImpl();
     }
+
+    @Test
+    void testEmptyListSize() {
+        assertEquals(0, circularList.size());
+    }
+
+    @Test
+    void testIsEmpty() {
+        assertEquals(true, circularList.isEmpty());
+    }
+
 
 }
