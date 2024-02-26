245a5ab10c64d9fc2cd65601635227daab61a0c6
Matteo <matteo.violani@studio.unibo.it>
2024-02-26 12:09:10 +0100

    fix(SimpleBankAccountWithAtmTest): refacotring, test more readable (removed magic numbers)

--- basic-refactoring-exercise/src/test/java/SimpleBankAccountWithAtmTest.java
+++ basic-refactoring-exercise/src/test/java/SimpleBankAccountWithAtmTest.java
@@ -9,39 +9,42 @@ import example.model.SimpleBankAccountWithAtm;
 
 public class SimpleBankAccountWithAtmTest {
     private final static double TRANSACTION_FEE = 1.0;
+    private final static double INITIAL_BALANCE = 0.0;
+    private final static double DEPOSIT_AMOUNT = 100.0;
+    private final static double WITHDRAW_AMOUNT = 50.0;
     private AccountHolder accountHolder;
     private BankAccount bankAccount;
 
     @BeforeEach
     void beforeEach() {
         this.accountHolder = new AccountHolder("Teo", "Viola", 0);
-        this.bankAccount = new SimpleBankAccountWithAtm(accountHolder, 0, (withdrawAmount)-> {
+        this.bankAccount = new SimpleBankAccountWithAtm(accountHolder, INITIAL_BALANCE, (withdrawAmount)-> {
             return TRANSACTION_FEE;
         });
     }
 
     @Test
     void testInitialBalance() {
-        assertEquals(0, bankAccount.getBalance());
+        assertEquals(INITIAL_BALANCE, bankAccount.getBalance());
     }
 
     @Test
     void testDepositWithFee() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        assertEquals(99, bankAccount.getBalance());
+        bankAccount.deposit(accountHolder.getId(), DEPOSIT_AMOUNT);
+        assertEquals(INITIAL_BALANCE+DEPOSIT_AMOUNT-TRANSACTION_FEE, bankAccount.getBalance());
     }
 
     @Test
     void testWrongDepositWithFee() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        bankAccount.deposit(2, 50);
-        assertEquals(99, bankAccount.getBalance());
+        testDepositWithFee();
+        bankAccount.deposit(2, DEPOSIT_AMOUNT);
+        assertEquals(INITIAL_BALANCE+DEPOSIT_AMOUNT-TRANSACTION_FEE, bankAccount.getBalance());
     }
 
     @Test
     void testWithdrawWithFee() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        bankAccount.withdraw(accountHolder.getId(), 50);
+        bankAccount.deposit(accountHolder.getId(), DEPOSIT_AMOUNT);
+        bankAccount.withdraw(accountHolder.getId(), WITHDRAW_AMOUNT);
         assertEquals(48, bankAccount.getBalance());
     }
 }
