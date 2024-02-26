e579109da716595c27d987c285eae6df0937656d
Matteo <matteo.violani@studio.unibo.it>
2024-02-26 17:30:45 +0100

    fix(CircularListImpl,CircularListTest): add single-multiple elements test

--- tdd-exercise/src/main/java/tdd/CircularListImpl.java
+++ tdd-exercise/src/main/java/tdd/CircularListImpl.java
@@ -1,18 +1,25 @@
 package tdd;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class CircularListImpl implements CircularList {
+    private List<Integer> list;
+    private int listIndex = 0;
+
+    public CircularListImpl() {
+        this.list = new ArrayList<>();
+    }
 
     @Override
     public void add(int element) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        this.list.add(element);
     }
 
     @Override
     public int size() {
-       return 0;
+       return this.list.size();
     }
 
     @Override
@@ -22,8 +29,9 @@ public class CircularListImpl implements CircularList {
 
     @Override
     public Optional<Integer> next() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'next'");
+        Integer element = this.list.get(this.listIndex);
+        nextElementIndex();
+        return Optional.of(element);
     }
 
     @Override
@@ -38,4 +46,7 @@ public class CircularListImpl implements CircularList {
         throw new UnsupportedOperationException("Unimplemented method 'reset'");
     }
 
+    private void nextElementIndex() {
+        this.listIndex = this.listIndex + 1 < this.size() ? this.listIndex + 1 : 0;
+    }
 }
--- tdd-exercise/src/test/java/CircularListTest.java
+++ tdd-exercise/src/test/java/CircularListTest.java
@@ -1,4 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -28,5 +33,27 @@ public class CircularListTest {
         assertEquals(true, circularList.isEmpty());
     }
 
+    @Test
+    void testAddOneElement() {
+        circularList.add(3);
+        assertAll(
+            ()-> assertEquals(Optional.of(3), circularList.next()),
+            () -> assertEquals(1, circularList.size())
+        );
+    }
 
+    @Test
+    void testMultipleAddElements() {
+        List<Integer> expectedList = List.of(0,1,4);
+        List<Integer> actuaList = new ArrayList<>();
+        expectedList.forEach(e -> circularList.add(e));
+
+        for(int index = 0; index < circularList.size(); index++) {
+            var nextElement = circularList.next();
+            if (nextElement.isPresent()) {
+                actuaList.add(nextElement.get());
+            }
+        }
+        assertEquals(expectedList, actuaList);
+    }
 }
