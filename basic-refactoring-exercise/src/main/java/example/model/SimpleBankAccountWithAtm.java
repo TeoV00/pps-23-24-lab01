884e4f3eb51e3e52a13584d67d7a8ebe635d8464
Matteo <matteo.violani@studio.unibo.it>
2024-02-26 11:14:41 +0100

    feat(SimpleBankAccountWithAtm,WithdrawFeeStrategy,SimpleBankAccountWithAtmTest): added bank account with fees

---
+++ basic-refactoring-exercise/src/main/java/example/model/SimpleBankAccountWithAtm.java
@@ -0,0 +1,21 @@
+package example.model;
+
+public class SimpleBankAccountWithAtm extends SimpleBankAccount {
+
+    private WithdrawFeeStrategy withdrawFeeStrategy;
+
+    public SimpleBankAccountWithAtm(AccountHolder holder, double balance, WithdrawFeeStrategy withdrawFeeStrategy) {
+        super(holder, balance);
+        this.withdrawFeeStrategy = withdrawFeeStrategy;
+    }
+
+    @Override
+    public void withdraw(int userID, double amount) {
+        super.withdraw(userID, amount + withdrawFeeStrategy.getFee());
+    }
+
+    @Override
+    public void deposit(int userID, double amount) {
+        super.deposit(userID, amount - withdrawFeeStrategy.getFee());
+    }
+}
---
+++ basic-refactoring-exercise/src/test/java/SimpleBankAccountWithAtmTest.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import example.model.AccountHolder;
+import example.model.BankAccount;
+import example.model.SimpleBankAccountWithAtm;
+
+public class SimpleBankAccountWithAtmTest {
+    private final static double TRANSACTION_FEE = 1.0;
+    private AccountHolder accountHolder;
+    private BankAccount bankAccount;
+
+    @BeforeEach
+    void beforeEach() {
+        this.accountHolder = new AccountHolder("Teo", "Viola", 0);
+        this.bankAccount = new SimpleBankAccountWithAtm(accountHolder, 0, ()-> {
+            return TRANSACTION_FEE;
+        });
+    }
+
+    @Test
+    void testInitialBalance() {
+        assertEquals(0, bankAccount.getBalance());
+    }
+
+    @Test
+    void testDepositWithFee() {
+        bankAccount.deposit(accountHolder.getId(), 100);
+        assertEquals(99, bankAccount.getBalance());
+    }
+
+    @Test
+    void testWrongDepositWithFee() {
+        bankAccount.deposit(accountHolder.getId(), 100);
+        bankAccount.deposit(2, 50);
+        assertEquals(99, bankAccount.getBalance());
+    }
+
+    @Test
+    void testWithdrawWithFee() {
+        bankAccount.deposit(accountHolder.getId(), 100);
+        bankAccount.withdraw(accountHolder.getId(), 50);
+        assertEquals(48, bankAccount.getBalance());
+    }
+}
