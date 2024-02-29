07e2b84f48388d5b3c9c806a0f3ebb76820572bc
Matteo <matteo.violani@studio.unibo.it>
2024-02-29 17:43:13 +0100

    doc: added java doc to interface

--- tdd-exercise/src/main/java/tdd3/FilteredCircularList.java
+++ tdd-exercise/src/main/java/tdd3/FilteredCircularList.java
@@ -2,11 +2,14 @@ package tdd3;
 
 import java.util.Optional;
 import java.util.function.Function;
-
 import tdd.CircularList;
 
 public interface FilteredCircularList extends CircularList{
 
+    /**
+     *  @param filter the filter used to search next element in circula list
+     * @return Optional.empty() if no element of list satisfy filter condition, otherwise an optional with the first element found.
+     * */
     Optional<Integer> filteredNext(Function<Integer, Boolean> filter);
 
 }
