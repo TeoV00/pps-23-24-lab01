4ad91fa4b9c90373dd3e15a55268290cf28878a8
Matteo <matteo.violani@studio.unibo.it>
2024-02-29 20:33:13 +0100

    doc(WithdrawFeeStrategy): added javadoc

--- basic-refactoring-exercise/src/main/java/example/model/WithdrawFeeStrategy.java
+++ basic-refactoring-exercise/src/main/java/example/model/WithdrawFeeStrategy.java
@@ -1,5 +1,11 @@
 package example.model;
 
 public interface WithdrawFeeStrategy {
+
+    /**
+     * Calculate fees
+     * @param withdrawAmount amount of money moved (deposited or withdrawed) per transaction
+     * @return the fees to be paid per transaction
+     */
     double getFee(double withdrawAmount);
 }
